2257a9859699d27e4897d3bde02874ac4331221c
axel <Axel@mail.lan>
2021-07-15 16:37:35 +0200

    authenticate

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const sequelize = new Sequelize(
         }
     }
 );
+
 /*
  * equivalent to: CREATE TABLE kamas_table(
  * id VARCHAR(255),
@@ -65,6 +66,12 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
     DB.sync().catch(e => {console.log(e)});
+    try {
+        sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
     console.log('Ready!');
     // DB.drop();
 });
